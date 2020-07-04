39e9e804f2d5fbe25037f84b614dc214226e3e79
Alison <alison.a.hernandez@gmail.com>
2020-07-04 01:01:43 -0400

    chore: creating barebones HomePage component

---
+++ client/components/home.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react'
+import {connect} from 'react-redux'
+import {getGameThunk, createGameThunk} from '../store/preGame'
+
+const HomePage = props => {
+  const handleJoin = () => {
+    console.log('handleJoin ran')
+    console.log('this is the game code being sent:', gameCode)
+    props.joinGame(gameCode)
+  }
+  const handleCreate = props => {
+    console.log('handleCreate ran')
+    props.createGame()
+  }
+
+  // React hooks! This functional component now has local state.
+  const [gameCode, setGameCode] = useState('')
+
+  const handleChange = event => {
+    setGameCode(event.target.value)
+    // console.log('game code on state:', gameCode)
+  }
+
+  return (
+    <div>
+      <div id="title">
+        <h1>[Code]opoly</h1>
+      </div>
+      <div id="textBox">
+        <div id="text">
+          Learn about the JavaScript stack! [Code]opoly looks like Monopoly, but
+          the win condition is totally different. Your goal is to build your
+          tech stack (think buying properties), earn seed money to fund your
+          startup, and reach GO to launch your company! Building your tech stack
+          isn’t easy; you’ll have to demonstrate your knowledge by answering
+          multiple choice questions, but beware of the miscellaneous hazards of
+          startup life! Oh, and don't catch any bugs!
+        </div>
+      </div>
+      <div id="buttonBox1">
+        <button onClick={handleJoin}>Join Game</button>
+        <input
+          type="text"
+          value={gameCode}
+          placeholder="game code"
+          onChange={handleChange}
+        />
+      </div>
+      <div id="buttonBox2">
+        <button onClick={handleCreate}>Create New Game</button>
+      </div>
+    </div>
+  )
+}
+
+const mapDispatch = dispatch => {
+  return {
+    joinGame: gameCode => {
+      dispatch(getGameThunk(gameCode))
+    },
+    createGame: () => {
+      dispatch(createGameThunk())
+    }
+  }
+}
+
+export default connect(null, mapDispatch)(HomePage)
---
+++ client/store/preGame.js
@@ -0,0 +1,86 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// This file might seem odd at first. Lots of thunks, only 1 action.
+// That's because the only thing we want in state preGame is the Game document.
+
+// Action Types:
+const GOT_GAME = 'GOT_GAME'
+
+// Action Creators:
+const gotGame = theGame => {
+  return {
+    type: GOT_GAME,
+    theGame
+  }
+}
+
+// Thunk Creators:
+// Used when a player presses JOIN GAME after selecting their character and startup name
+export const createPlayerThunk = (gameCode, startupName, characterImg) => {
+  return async (dispatch, getState, {getFirebase}) => {
+    console.log('createPlayerThunk is running')
+    try {
+      // First make a player:
+      console.log('if this ran, a player was made!')
+      await getFirebase()
+        .firestore()
+        .collection('players')
+        .add({
+          startupName: startupName,
+          image: characterImg,
+          game_id: gameCode,
+          seedMoney: 500,
+          location: 1,
+          hasFrontend: 'none',
+          hasBackend: 'none',
+          hasUI: 'none',
+          hasMiddleware: 'none',
+          hasAlgorithm: 'none'
+        })
+      // Here we do not dispatch an action creator;
+      // I'm dispatching another thunk (trying to keep this thunk lightweight)
+      dispatch(getGameThunk())
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+// Used when a player presses JOIN GAME from the home screen AND at player creation screen
+export const getGameThunk = gameCode => {
+  return async (dispatch, getState, {getFirebase}) => {
+    // Grab the game, now containing the newly created player
+    try {
+      console.log('if this ran, a game was read from Firestore!')
+      const theGame = await getFirebase()
+        .firestore()
+        .collection('codeopoly')
+        .doc('1')
+        .collection('game')
+        .doc(gameCode)
+        .get()
+      if (!theGame.exists) {
+        console.log('invalid game code!!!')
+      } else {
+        console.log('dispatching gotGame', theGame.data())
+        dispatch(gotGame(theGame.data()))
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+// Reducer:
+export default function(state = {}, action) {
+  console.log('the reducer is being accessed!')
+  switch (action.type) {
+    case GOT_GAME:
+      console.log('theGame inside the reducer', action.theGame)
+      return action.theGame
+    default:
+      return state
+  }
+}
