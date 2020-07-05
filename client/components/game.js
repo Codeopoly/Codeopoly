e381705b9df1a674df4a5647691946bd58c6e011
Alison <alison.a.hernandez@gmail.com>
2020-07-05 18:42:48 -0400

    chore: phaser game loads on /game route and disappears when navigating away

--- client/components/game.js
+++ client/components/game.js
@@ -1,38 +1,45 @@
-import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
-import React from "react";
-import ReactDOM from "react-dom";
+import Phaser from 'phaser'
+import React, {useEffect} from 'react'
+import ReactDOM from 'react-dom'
 
 const Game = () => {
+  useEffect(() => {
+    console.log('useEffect ran!')
+    return function cleanup() {
+      console.log('cleanup ran!', game)
+      game.destroy(true)
+    }
+  })
   const config = {
     type: Phaser.AUTO,
-    parent: "phaser-example",
+    parent: 'phaser-example',
     width: 800,
     height: 600,
     scene: {
       preload: preload,
       create: create
     }
-  };
-  
-  const game = new Phaser.Game(config);
-  
-  function preload() {
-    this.load.image("logo", logoImg);
   }
-  
+
+  const game = new Phaser.Game(config)
+
+  function preload() {
+    this.load.image('logo', 'assets/logo.png')
+  }
+
   function create() {
-    const logo = this.add.image(400, 150, "logo");
-  
+    const logo = this.add.image(400, 150, 'logo')
+
     this.tweens.add({
       targets: logo,
       y: 450,
       duration: 2000,
-      ease: "Power2",
+      ease: 'Power2',
       yoyo: true,
       loop: -1
-    });
-  }  
+    })
+  }
+  return null
 }
 
-export default Game;
+export default Game
