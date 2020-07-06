20bdc0adc1b5d14e891e0209bcd6ab1340bc0bfd
nora <norakrantz1@gmail.com>
2020-07-06 19:27:06 -0400

    losing files

--- client/components/waitForPlayers.js
+++ client/components/waitForPlayers.js
@@ -2,14 +2,16 @@ import React, {useState, useEffect} from 'react'
 import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
 import preGame from '../store/preGame'
-// import { useParams } from 'react-router';
+import {getGameSnapshot} from '../store/game'
 
 const WaitForPlayers = props => {
   const [host, setHost] = useState('')
+  const [players, setPlayers] = useState([])
 
   useEffect(() => {
     setHost(props.preGame.game.host)
-    console.log('useEffect props.preGame', props.preGame)
+    getGameSnapshot(props.preGame.gameCode)
+    console.log('useEffect props.preGame', props.preGame.gameCode)
   })
 
   console.log('props', props)
@@ -19,13 +21,21 @@ const WaitForPlayers = props => {
       <div id="title">
         <h1>[Code]opoly</h1>
         <h4>Room Code: {props.match.params.gameId}</h4>
+        {players[0] ? (
+          players.map(player => {
+            return <p key="player.id">player: {player.id}</p>
+          })
+        ) : (
+          <p>No players have joined yet</p>
+        )}
       </div>
       <div id="playersJoinedBox">
         {host ? (
           host.includes(props.preGame.playerId) ? (
             <button type="button">Start Game</button>
           ) : (
-            <p>Host ID: {host.slice(8, host.length)}</p>
+            (<p>Host ID: {host.slice(8, host.length)}</p>,
+            <p>Waiting for host to start game...</p>)
           )
         ) : (
           <br />
@@ -41,6 +51,12 @@ const mapState = state => {
   }
 }
 
-const mapDispatch = dispatch => {}
+const mapDispatch = dispatch => {
+  return {
+    getGameSnapshot: gameId => {
+      dispatch(getGameSnapshot(gameId))
+    }
+  }
+}
 
 export default connect(mapState, mapDispatch)(WaitForPlayers)
