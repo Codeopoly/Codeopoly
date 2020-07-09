89a1226671923218fb41b54e2489c8a643b45b88
Kitty Xu <kittyxu725@gmail.com>
2020-07-09 13:05:37 -0400

    feat: trying to add tilemap to phaser

--- client/components/scene.js
+++ client/components/scene.js
@@ -18,6 +18,8 @@ export default class SceneMain extends Phaser.Scene {
     this.aGrid = new AlignGrid(gridConfig, this.game)
     this.aGrid.showNumbers()
 
+    const arr = []
+
     // this.center = this.add.image(
     //   this.game.config.width / 2, // where the center of the image is placed on the x-axis
     //   this.game.config.height / 2, // y-axis
@@ -28,14 +30,21 @@ export default class SceneMain extends Phaser.Scene {
     // this.center.displayWidth = this.game.config.width * 0.665
     // this.center.scaleY = this.center.scaleX
 
-    const backgroundImage = this.add.image(
-      this.game.config.width / 2,
-      this.game.config.height / 2,
-      'background'
-    )
+    // const backgroundImage = this.add.image(
+    //   this.game.config.width / 2,
+    //   this.game.config.height / 2,
+    //   'background'
+    // )
     // .setOrigin(0, 0)
     // backgroundImage.setScale(2, 0.8)
-    const board = this.make.tilemap({key: 'board'})
+    // const board = this.make.tilemap({key: 'board'})
+    let board = this.make.tilemap({key: 'board'})
+    // let boardData = board.addTilesetImage('board')
+    console.log('board is added')
+    const tileset = board.addTilesetImage('background')
+    console.log('tileset added')
+    const layer = board.createStaticLayer('Tile Layer 1', tileset, 0, 0)
+    console.log('layer added')
 
     //place the face on the grid
     //this.aGrid.placeAtIndex(65.5, this.center);
