126b0ff87801ac8e03430f44ce9429d67743555b
nora <norakrantz1@gmail.com>
2020-07-09 22:45:34 -0400

    added navbar buttons

---
+++ client/components/instructions.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Instructions = () => {
+  return (
+    <div className="welcome">
+      <div id="title">
+        <h1>[Code]opoly Instructions</h1>
+      </div>
+      <div id="textBox">
+        <div id="text">Rule #1: Don't be an asshole</div>
+      </div>
+    </div>
+  )
+}
+
+export default Instructions
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -11,27 +11,12 @@ import 'firebase/auth'
 const Navbar = ({handleClick, isLoggedIn, handleTest, handleTest2}) => (
   <div>
     <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <button type="button" onClick={handleTest}>
-            Test
-          </button>
-          <button type="button" onClick={handleTest2}>
-            get all players
-          </button>
-        </div>
-      )}
+      <Link to="/">
+        <button type="button">Play</button>
+      </Link>
+      <Link to="/instructions">
+        <button type="button">Instructions</button>
+      </Link>
     </nav>
     <hr />
   </div>
