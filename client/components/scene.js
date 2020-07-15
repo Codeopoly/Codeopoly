554504e7489ac340e9ec1bcef8f8f48c2fce569e
Venessa <v_campbell10@yahoo.com>
2020-07-15 01:12:30 -0400

    added new tile maps

--- client/components/scene.js
+++ client/components/scene.js
@@ -1,4 +1,6 @@
+/* eslint-disable max-statements */
 import AlignGrid from '../../utility/alignGrid'
+import {auth} from 'firebase'
 
 export default class SceneMain extends Phaser.Scene {
   constructor() {
@@ -21,7 +23,6 @@ export default class SceneMain extends Phaser.Scene {
       frameWidth: 425,
       frameHeight: 275
     })
-
   }
   create() {
     this.keys = this.input.keyboard.createCursorKeys()
@@ -37,41 +38,134 @@ export default class SceneMain extends Phaser.Scene {
     // this.physics.world.bounds.width = board.width
     // this.physics.world.bounds.height = board.height
 
-    const interviewLayer = board.createStaticLayer('Interview', tileset, 0, 0)
-    console.log('interview layer added')
-
-    const interviewTiles = board.createFromObjects('Interview')
+    //getting object layers from json file
     const interviewObj = board.getObjectLayer('Interview').objects
     const drawCardObj = board.getObjectLayer('Draw a card').objects
-    //console.log below works!!
-    console.log('these are two object layers', interviewObj, drawCardObj)
+    const coffeeBreakObj = board.getObjectLayer('Coffee Break').objects[0]
+    const stealSomeTechObj = board.getObjectLayer('Steal some tech').objects[0]
+    const queuesObj = board.getObjectLayer('Queues').objects[0]
+    const stacksObj = board.getObjectLayer('Stacks').objects[0]
+    const linkedListsObj = board.getObjectLayer('Linked lists').objects[0]
+    const treesObj = board.getObjectLayer('Trees').objects[0]
+    const graphsObj = board.getObjectLayer('Graphs').objects[0]
+    const gotaBugObj = board.getObjectLayer('Got a bug').objects[0]
+    const newInvestorObj = board.getObjectLayer('New Investor').objects[0]
+    const sequelizeObj = board.getObjectLayer('Sequelize').objects[0]
+    const firebaseObj = board.getObjectLayer('Firebase').objects[0]
+    const expressObj = board.getObjectLayer('Express').objects[0]
+    const mongoDBObj = board.getObjectLayer('MongoDB').objects[0]
+    const loseMoneyObj = board.getObjectLayer('Lose money').objects[0]
+    const middlewareObj = board.getObjectLayer('Middleware').objects[0]
+    const authObj = board.getObjectLayer('Auth').objects[0]
+    const codeDesignObj = board.getObjectLayer('Code design').objects[0]
+    const materialUIObj = board.getObjectLayer('MaterialUI').objects[0]
+    const cssObj = board.getObjectLayer('CSS').objects[0]
+    const htmlObj = board.getObjectLayer('HTML').objects[0]
+    const stuckOnBugObj = board.getObjectLayer('Stuck on bug').objects[0]
+    const semanticUIObj = board.getObjectLayer('SemanticUI').objects[0]
+    const reactObj = board.getObjectLayer('React').objects[0]
+    const reactNativeObj = board.getObjectLayer('React native').objects[0]
+    const emberObj = board.getObjectLayer('Ember').objects[0]
+    const vueObj = board.getObjectLayer('Vue').objects[0]
+    const angularObj = board.getObjectLayer('Angular').objects[0]
+    const goObj = board.getObjectLayer('Go').objects[0]
 
-    //THIS CONSOLE LOGS!!
+    this.singleTilesArr = [
+      goObj,
+      loseMoneyObj,
+      mongoDBObj,
+      expressObj,
+      firebaseObj,
+      sequelizeObj,
+      newInvestorObj,
+      gotaBugObj,
+      graphsObj,
+      treesObj,
+      linkedListsObj,
+      stacksObj,
+      queuesObj,
+      stealSomeTechObj,
+      coffeeBreakObj,
+      angularObj,
+      vueObj,
+      emberObj,
+      reactNativeObj,
+      reactObj,
+      semanticUIObj,
+      stuckOnBugObj,
+      htmlObj,
+      cssObj,
+      materialUIObj,
+      codeDesignObj,
+      authObj,
+      middlewareObj
+    ]
 
-    this.overlapObjectsGroup = this.physics.add.group({})
+    console.log('checking draw card props', drawCardObj)
 
+    //CREATING GROUPS FOR TILES BELOW
+    this.interviewGroup = this.physics.add.group({})
+    this.drawCardGroup = this.physics.add.group({})
+
+    this.interviewArr = []
     interviewObj.forEach(object => {
-      console.log('HI THIS IS WORKING I GUESS????')
-      let obj = this.overlapObjectsGroup.create(object.x, object.y, 'tile')
+      console.log('INTERVIEW PLACEMENT WORKS')
+      let obj = this.interviewGroup.create(object.x, object.y, 'tile')
       obj.setOrigin(0)
       obj.body.width = object.width
       obj.body.height = object.height
+      this.interviewArr.push(obj)
+    })
+    this.drawCardArr = []
+    drawCardObj.forEach(object => {
+      console.log('DRAW CARD PLACEMENT WORKS')
+      let obj = this.drawCardGroup.create(object.x, object.y, 'tile')
+      obj.setOrigin(0)
+      obj.body.width = object.width
+      obj.body.height = object.height
+      this.drawCardArr.push(obj)
     })
 
-    // overlapObjectsGroup.refresh()
+    //SINGLE TILE PLACEMENT BELOW
+    this.placementArr = []
+    this.singleTilesArr.forEach(object => {
+      let obj = this.physics.add.sprite(object.x, object.y, object.name)
+      obj.setOrigin(0)
+      obj.body.width = object.width
+      obj.body.height = object.height
+      this.placementArr.push(obj)
+    })
 
-    // this.physics.add.overlap(this.doge, this.overlapObjectsGroup)
+    // console.log(this.placementArr)
 
+    // PATH TILES IN ORDER FROM GO TO FINISH
+    this.tilePathArr = this.placementArr
 
-    // const drawCardLayer = board.createStaticLayer('Draw a card', tileset, 0, 0)
-    // console.log('draw card layer added')
+    //insert draw card right at 6th idx
+    this.tilePathArr.splice(6, 0, this.drawCardArr[1])
+    //insert interview right at 8th idx
+    this.tilePathArr.splice(8, 0, this.interviewArr[1])
+    //insert interview top at 13th idx
+    this.tilePathArr.splice(13, 0, this.interviewArr[0])
+    //insert draw card top at 16th idx
+    this.tilePathArr.splice(16, 0, this.drawCardArr[0])
+    //insert interview left at 20th idx
+    this.tilePathArr.splice(20, 0, this.interviewArr[2])
+    //insert draw card left at 24th idx
+    this.tilePathArr.splice(24, 0, this.drawCardArr[2])
+    //insert interview bottom at 31st idx
+    this.tilePathArr.splice(31, 0, this.interviewArr[3])
+    //insert draw card bottom at 32nd idx
+    this.tilePathArr.splice(32, 0, this.drawCardArr[3])
+    console.log(this.tilePathArr)
+
+    //DOGE PLACEMENT BELOW
 
     this.doge = this.physics.add.sprite(0, 0, 'doge')
     this.doge.setOrigin(-10.5, -10)
     this.doge.setScale(0.07)
 
     // this.doge.setCollideWorldBounds(true) // don't go out of the map
-
 
     // Code for the dice:
     const first = this.add.sprite(180, 200, 'dice')
@@ -88,7 +182,6 @@ export default class SceneMain extends Phaser.Scene {
 
     // dice animations
     const anim1 = this.anims.create({
-
       key: 'firstDiceRoll',
       repeat: -1,
       frameRate: 15 + Math.floor(Math.random() * 3), // randomness to avoid the same rolling patterns
@@ -236,7 +329,7 @@ export default class SceneMain extends Phaser.Scene {
     // End Callstack Deck code.
   }
   update() {
-    // this.overlapObjectsGroup(this.doge)
+    // this.interviewGroup(this.doge)
     //SPRITE ANIMATION
     let spriteMovement = {velocity: 8}
 
@@ -253,13 +346,15 @@ export default class SceneMain extends Phaser.Scene {
       this.doge.y -= spriteMovement.velocity
     }
 
-    this.physics.add.overlap(
-      this.doge,
-      this.overlapObjectsGroup,
-      this.activateFunc,
-      null,
-      this
-    )
+    this.placementArr.forEach(placement => {
+      this.physics.add.overlap(
+        this.doge,
+        placement,
+        this.activateFunc,
+        null,
+        this
+      )
+    })
   }
   activateFunc(player, tile) {
     console.log('inside the func')
